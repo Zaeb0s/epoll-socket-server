36bd9b7fc2c6c0ee6dd770d9fbe38309c16e8e62
Ze3b0s <chrisse.zakrisson@gmail.com>
2015-12-09 14:24:09 +0100

    Changed wrong name

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/bin/env python3
 from distutils.core import setup
 setup(
-  name = 'EScocketS',
+  name = 'ESocketS',
   packages = ['ESocketS'], # this must be the same as the name above
   version = '0.1.1',
   description = 'A socket server using select.epoll',
   author = 'Christoffer Zakrisson',
   author_email = 'christoffer_zakrisson@hotmail.com',
   url = 'https://github.com/Zaeb0s/epoll-socket-server', # use the URL to the github repo
-  download_url = 'https://github.com/Zaeb0s/epoll-socket-server/tarball/0.1.1', # I'll explain this in a second
   keywords = ['socket', 'epoll', 'server', 'poll', 'select', 'TCP', 'web'], # arbitrary keywords
   classifiers = [],
 )
