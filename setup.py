68ec19bfeec3204d202100cce078a88d63281323
Ze3b0s <chrisse.zakrisson@gmail.com>
2015-12-09 13:15:42 +0100

    Changed url in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'A socket server using select.epoll',
   author = 'Christoffer Zakrisson',
   author_email = 'christoffer_zakrisson@hotmail.com',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  url = 'https://github.com/Zaeb0s/epoll-socket-server', # use the URL to the github repo
   download_url = 'https://github.com/Zaeb0s/epoll-socket-server/tarball/0.1', # I'll explain this in a second
   keywords = ['socket', 'epoll', 'server', 'poll', 'select', 'TCP', 'web'], # arbitrary keywords
   classifiers = [],
