76300a2a5a7f9dd408122a397a210237b6dfc651
Zaeb0s <christoffer_zakrisson@hotmail.com>
2015-12-17 15:10:36 +0100

    LICENSE is not MIT

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/bin/env python3
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'ESocketS',
   packages = ['ESocketS'], # this must be the same as the name above
-  version = '0.1.8',
+  version = '0.2.0',
+  license = 'MIT',
   description = 'A socket server using select.epoll',
   author = 'Christoffer Zakrisson',
   author_email = 'christoffer_zakrisson@hotmail.com',
   url = 'https://github.com/Zaeb0s/epoll-socket-server', # use the URL to the github repo
   keywords = ['socket', 'epoll', 'server', 'poll', 'select', 'TCP', 'web'], # arbitrary keywords
-  classifiers = [],
+  classifiers = ['Development Status :: 4 - Beta',
+                 'Programming Language :: Python :: 3.5',
+                 'Operating System :: POSIX :: Linux',
+                 'License :: OSI Approved :: MIT License'],
 )
