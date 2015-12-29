81a257aecf991e80cb976f2117384c6e6d60e682
Zaeb0s <christoffer_zakrisson@hotmail.com>
2015-12-29 14:54:19 +0100

    Now uses loopfunction

--- ESocketS/socket_server.py
+++ ESocketS/socket_server.py
@@ -2,58 +2,28 @@
 import selectors
 import queue
 import socket
-import threading
-
-
-class _ServeForever:
-    """Polls for new clients then accepts and puts them into the clients queue.Queue object
-    server_socket is a bound and listening socket.socket object
-    """
-
-    def __init__(self, function, *args, **kwargs):
-
-        self.function = function
-        self.args = args
-        self.kwargs = kwargs
-
-        self._stop_signal = False
-
-        self._lock = threading.Event()
-        self._lock.set()
-
-    def _loop(self):
-        self._lock.clear()
-        try:
-            while not self._stop_signal:
-                self.function(*self.args, **self.kwargs)
-
-        finally:
-            self._lock.set()
-
-    def start(self):
-        if self.is_running():
-            raise RuntimeError('Mainloop is already running')
-        else:
-            self._stop_signal = False
-            self._loop()
-
-    def stop(self):
-        self._stop_signal = True
-        self._lock.wait()
-
-    def send_stop_signal(self):
-        self._stop_signal = True
-
-    def restart(self):
-        self.stop()
-        self.__init__(self.function, *self.args, **self.kwargs)
-        self.start()
-
-    def is_running(self):
-        return not self._lock.is_set()
-
+import loopfunction
+import logging
+import traceback
 
 class SocketServer:
+
+    def log(f):
+        def wrapper(*args, **kwargs):
+            logging.info('function {} called'.format(f.__name__))
+            try:
+                f(*args, **kwargs)
+            except:
+                logging.error('function {} exited with error:\n'.format(f.__name__) +
+                              '-'*50 + '\n' +
+                              traceback.format_exc() +
+                              '-'*50 + '\n')
+                raise
+            else:
+                logging.info('function {} exited normally'.format(f.__name__))
+        return wrapper
+
+    @log
     def __init__(self,
                  port=1234,
                  host=socket.gethostbyname(socket.gethostname()),
@@ -62,7 +32,7 @@ class SocketServer:
                  selector=selectors.EpollSelector):
 
         self.port = port
-        self.host = socket.gethostbyname(socket.gethostname())
+        self.host = host
         self.queue_size = queue_size
         self.block_time = block_time
         self.selector = selector
@@ -79,15 +49,26 @@ class SocketServer:
 
         self._accept_selector.register(self._server_socket, selectors.EVENT_READ)
 
-        self._server1 = _ServeForever(self._search_incoming)
-        self._server2 = _ServeForever(self._search_readable)
-        self._server3 = _ServeForever(self._handle_incoming)
-        self._server4 = _ServeForever(self._handle_readable)
+        self._server1 = loopfunction.Loop(target=self._search_incoming,
+                                          on_stop=lambda:
+                                          logging.info('search for incoming clients thread stopped'))
+        self._server2 = loopfunction.Loop(target=self._search_readable,
+                                          on_stop=lambda:
+                                          logging.info('search for readable client sockets thread stopped'))
+        self._server3 = loopfunction.Loop(target=self._handle_incoming,
+                                          on_stop=lambda:
+                                          logging.info('handle incoming clients thread stopped'))
+        self._server4 = loopfunction.Loop(target=self._handle_readable,
+                                          on_stop=lambda:
+                                          logging.info('handle readable client sockets thread stopped'))
+
+        self.clients = {}
 
     def _search_incoming(self):
         if self._accept_selector.select(timeout=self.block_time):
             try:
                 client = self._server_socket.accept()
+                logging.info('Client connected: {}'.format(client[1]))
                 self._accept_queue.put(client)
             except socket.error:
                 pass
@@ -102,164 +83,110 @@ class SocketServer:
     def _handle_incoming(self):
         try:
             client, address = self._accept_queue.get(timeout=self.block_time)
-            client.setblocking(False)
-            self.handle_incoming(client, address)
+            if self.handle_incoming(client, address):
+                logging.info('Accepted connection from client: {}'.format(address))
+                client.setblocking(False)
+                self.clients[client] = address
+                self.register(client)
+            else:
+                logging.info('Refused connection from client: {}'.format(address))
+                self.disconnect(client)
         except queue.Empty:
             pass
 
     def _handle_readable(self):
         try:
             client = self._recv_queue.get(timeout=self.block_time)
-            self.handle_readable(client)
+            if self.handle_readable(client):
+                self.register(client)
+            else:
+                self.disconnect(client)
         except queue.Empty:
             pass
 
-    def handle_readable(self, client):
-        pass
-
     def handle_incoming(self, client, address):
-        pass
+        return True
 
+    def handle_readable(self, client):
+        return True
+
+
+
+    @log
     def start(self):
-        try:
-            self._server_socket.bind((self.host, self.port))
-        except OSError:
-            pass
+        logging.info('Binding server socket to {}:{}'.format(self.host, self.port))
+        self._server_socket.bind((self.host, self.port))
+
         self._server_socket.listen(self.queue_size)
+        logging.info('Server socket now listening (queue_size={})'.format(self.queue_size))
 
-        threading.Thread(target=self._server1.start).start()
-        threading.Thread(target=self._server2.start).start()
-        threading.Thread(target=self._server3.start).start()
-        threading.Thread(target=self._server4.start).start()
+        logging.info('Starting main threads...')
+        self._server1.start(subthread=True)
+        logging.info('Now searching for incoming client connections')
+        self._server2.start(subthread=True)
+        logging.info('Now searching for readable client sockets')
+        self._server3.start(subthread=True)
+        logging.info('Now handling incoming client connections')
+        self._server4.start(subthread=True)
+        logging.info('Now handling readable client sockets')
+        logging.info('Main threads started')
 
+    @log
     def stop(self):
+        logging.info('Closing all ({}) connections...'.format(len(self.clients)))
+
+        self.disconnect(self.clients)
+        logging.info('Stopping mainthreads...')
         self._server1.send_stop_signal()
         self._server2.send_stop_signal()
         self._server3.send_stop_signal()
         self._server4.send_stop_signal()
-
         self._server1.stop()
         self._server2.stop()
         self._server3.stop()
         self._server4.stop()
+        logging.info('Closing server socket...')
+        self._server_socket.shutdown(socket.SHUT_RDWR)
+        self._server_socket.close()
 
-    def register(self, client):
-        self._recv_selector.register(client, selectors.EVENT_READ)
+    def register(self, client, silent=False):
+        try:
+            self._recv_selector.register(client, selectors.EVENT_READ)
+        except KeyError:
+            if not silent:
+                logging.error(
+                    'Tried to register an already registered client: {}'.format(self.clients[client]))
+                raise KeyError('Client already registered')
 
+    def unregister(self, client, silent=False):
+        try:
+            self._recv_selector.unregister(client)
+        except KeyError:
+            if not silent:
+                logging.error(
+                    'Tried to unregister a client that is not registered: {}'.format(self.clients[client]))
+                raise KeyError('Client already registered')
 
-# class AcceptServer:
-#     def __init__(self,
-#                  server_socket,
-#                  block_time=2,
-#                  selector=selectors.PollSelector):
-#
-#         self.server_socket = server_socket
-#         self.block_time = block_time
-#         self.selector = selector()
-#
-#         self.server = ServeForever(self.accept, self)
-#         self.selector.register(server_socket, selectors.EVENT_READ)
-#         self._queue = queue.Queue()
-#
-#     def accept(self):
-#         if self.selector.select(self.block_time):
-#             try:
-#                 client = self.server_socket.accept()
-#             except socket.error:
-#                 pass
-#             else:
-#                 self._queue.put(client)
-#             finally:
-#                 self.server.stop()
-#
-#     def get(self, timeout):
-#         return self._queue.get(timeout=timeout)
-#
-#
-# class RecvServer(ServeForever):
-#     def __init__(self,
-#                  block_time=2,
-#                  selector=selectors.PollSelector):
-#
-#         self.block_time = block_time
-#         self.selector = selector()
-#
-#         ServeForever.__init__(self)
-#
-#         self._queue = queue.Queue()
-#
-#     def loop_function(self):
-#         events = self.selector.select(self.block_time)
-#         for key, mask in events:
-#             self.selector.unregister(key.fileobj)
-#             self._queue.put(key.fileobj)
-#
-#     def get(self, timeout):
-#         return self._queue.get(timeout=timeout)
-#
-#
-#
-# class handleQueue(ServeForever):
-#     def __init__(self,
-#                  thequeue,
-#                  block_time=2):
-#
-#         self.block_time = block_time
-#
-#         ServeForever.__init__(self)
-#
-#         self._queue = thequeue
-#
-#     def loop_function(self):
-#         data = self._queue.get(timeout=self.block_time)
-#
-#
-#
-# class SocketServer:
-#     def __init__(self,
-#                  port=1234,
-#                  host=socket.gethostbyname(socket.gethostname()),
-#                  queue_size=1000,
-#                  block_time=2,
-#                  selector=selectors.PollSelector):
-#
-#         self.host = host
-#         self.port = port
-#         self.queue_size = queue_size
-#
-#         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#         self._server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#         self._server_socket.setblocking(False)
-#
-#         self._accept_server = AcceptServer(self._server_socket,
-#                                           block_time,
-#                                           selector)
-#
-#         self._recv_server = RecvServer(block_time,
-#                                       selector)
-#
-#     def register(self, client):
-#         self._recv_server.selector.register(client, selectors.EVENT_READ)
-#
-#     def unregister(self, client):
-#         self._recv_server.selector.unregister(client)
-#
-#     def start(self):
-#         self._accept_server.start()
-#         self._recv_server.start()
-#
-#     def _handle_recv(self):
-#         try:
-#
-#     def handle_recv(self):
-#         pass
-#
-#     def handle_accept(self):
-#         pass
-#
-#
-# class NormalStop(Exception):
-#     pass
-#
-# class ErrorStop(Exception):
-#     pass
+    def disconnect(self, client, how=socket.SHUT_RDWR):
+        if hasattr(client, '__iter__'):
+            if client == self.clients:
+                client = self.clients.copy()
+            for i in client:
+                self.disconnect(i, how)
+
+        else:
+            self.unregister(client, True)
+            address = 'Could not find address'
+            try:
+                address = client.getpeername()
+                client.shutdown(socket.SHUT_RDWR)
+            except socket.error:
+                pass
+            client.close()
+
+            try:
+                address = self.clients[client]
+                del self.clients[client]
+            except KeyError:
+                pass
+            logging.info('Client disconnected: {}'.format(address))
--- test.py
+++ test.py
@@ -3,28 +3,36 @@ import ESocketS
 import threading
 import socket
 import time
+import logging
+import sys
+
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+root.addHandler(ch)
+
 
 class EchoServer(ESocketS.SocketServer):
-    clients = {}
 
     def handle_incoming(self, client, address):
-        # print(address, 'Connected')
-        self.clients[client] = address
-        self.register(client)
+        return False
 
     def handle_readable(self, client):
         data = client.recv(1028)
         if data == b'':
-            print(client.getpeername(), 'Disconnected')
-            return
+            return False
 
-        print(client.getpeername(), data)
+        print(self.clients[client], data)
         self.register(client)
         client.sendall(b'SERVER: ' + data)
-
+        return True
 
 s = EchoServer()
-s.start()
+# s.start()
 
 # clients = []
 # sent = 0
@@ -37,8 +45,8 @@ def connect_clients(no_clients):
     t1 = time.time()
     for i in range(no_clients):
         threading.Thread(target=connect).start()
-    while len(temp.clients) != no_clients:
-        pass
+    # while len(temp.clients) != no_clients:
+    #     pass
     return time.time() - t1
 
 
@@ -55,8 +63,26 @@ def connect():
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     conn.connect((s.host, s.port))
-    temp.clients.append(conn)
+    conn.sendall(b'hej '*500)
+    conn.shutdown(socket.SHUT_RDWR)
+    conn.close()
+    # temp.clients.append(conn)
 
 def send(client, msg):
     client.sendall(msg)
     temp.sent += 1
+
+#
+# class logg:
+#     def __init__(self, f):
+#         self.f = f
+#
+#     def __call__(self, *args, **kwargs):
+#         logging.info('function {} called'.format(self.f.__name__))
+#         self.f(*args, **kwargs)
+#         logging.info('function {} exited'.format(self.f.__name__))
+#
+#
+# @logg
+# def party():
+#     print('hi')
