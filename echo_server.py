3cfb18df1152efece453b04fbb7a6211dae451ed
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-02-22 01:47:55 +0100

    ESocketS -> esockets

--- echo_server.py
+++ echo_server.py
@@ -11,31 +11,31 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
 ch.setFormatter(formatter)
 root.addHandler(ch)
 
-# def handle_incoming(client, address):
-#     """
-#     Return True: The client is accepted and the server starts polling for messages
-#     Return False: The server disconnects the client.
-#     """
-#
-#     client.sendall(b'SERVER: Connection accepted!\n')
-#     return True
-#
-# def handle_readable(client):
-#     """
-#     Return True: The client is re-registered to the selector object.
-#     Return False: The server disconnects the client.
-#     """
-#
-#     data = client.recv(1028)
-#     if data == b'':
-#         return False
-#     client.sendall(b'SERVER: ' + data)
-#     return True
-#
-# server = ESocketS.SocketServer(handle_incoming=handle_incoming,
-#                                handle_readable=handle_readable)
-# server.start()
-# print('Server started on: {}:{}'.format(server.host, server.port))
+def handle_incoming(client, address):
+    """
+    Return True: The client is accepted and the server starts polling for messages
+    Return False: The server disconnects the client.
+    """
 
-server = esockets.SocketServer()
+    client.sendall(b'SERVER: Connection accepted!\n')
+    return True
+
+def handle_readable(client):
+    """
+    Return True: The client is re-registered to the selector object.
+    Return False: The server disconnects the client.
+    """
+
+    data = client.recv(1028)
+    if data == b'':
+        return False
+    client.sendall(b'SERVER: ' + data)
+    return True
+
+server = esockets.SocketServer(handle_incoming=handle_incoming,
+                               handle_readable=handle_readable)
 server.start()
+print('Server started on: {}:{}'.format(server.host, server.port))
+
+# server = esockets.SocketServer()
+# server.start()
