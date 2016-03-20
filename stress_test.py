84f5893543331704d8e1b8978f2a1bbf78cd2e13
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-03-21 00:04:48 +0100

    again sending on connect

--- stress_test.py
+++ stress_test.py
@@ -37,11 +37,11 @@ message = b'hello server'
 host = '192.168.1.7'
 port = 8000
 def connect(host, port):
-    sleep(random()/100)
+    # sleep(random()/100)
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.connect((host, port))
-    # sock.recv(1)
+    sock.recv(1)
     return sock
 
 
@@ -62,7 +62,7 @@ def users(no_users):
 
 # threading.Thread(target=send_messages).start()
 
-for i in range(20):
+for i in range(200):
     threading.Thread(target=users, args=(5000,)).start()
 
 
