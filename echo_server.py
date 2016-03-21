264be79c49306c4435b4d75293870eae2850c230
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-03-21 03:48:04 +0100

    changed logginglevel

--- echo_server.py
+++ echo_server.py
@@ -3,7 +3,7 @@ import esockets
 import logging, sys
 import socket
 root = logging.getLogger()
-root.setLevel(logging.ERROR)
+root.setLevel(logging.INFO)
 ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(levelname)s - %(message)s')
