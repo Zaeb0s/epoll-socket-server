ab7d842e83b25310c860bfd495c38c9760c08272
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-03-21 23:23:41 +0100

    test

--- stress_test.py
+++ stress_test.py
@@ -34,8 +34,9 @@ message = b'hello server'
 #         sleep(max(0, 1/messages_per_second - (t2-t1)))
 
 
-# host = '192.168.1.28'
-host = 'zaebos.ddns.net'
+
+# host = 'zaebos.ddns.net'
+host = '192.168.1.28'
 # host = '213.113.6.13'
 ports = range(8000, 8050)
 
